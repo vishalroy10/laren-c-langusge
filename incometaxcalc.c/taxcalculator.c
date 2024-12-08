89e2f4f35b838fc32ef3e07025179f0a803ac80b
vishalroy10 <vishalroy00004@gmail.com>
2024-12-08 04:45:27 -0800

    thid is tax calculator

--- incometaxcalc.c/taxcalculator.c
+++ incometaxcalc.c/taxcalculator.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
-int main(){
-    int income, tax = 0;
-    printf("Enter income : ");
-    scanf(("%d\n", &income));
-    if(income<=250000){
+int main() {
+    int income;
+    float tax = 0; // Changed to float to handle decimal values
+
+    printf("Enter income: ");
+    scanf("%d", &income); // Corrected scanf
+
+    // Tax calculation based on income brackets
+    if (income <= 250000) {
         tax = 0;
-    }
-    else if(income >250000 && income<=500000){
+    } else if (income > 250000 && income <= 500000) {
         tax = 0.05 * (income - 250000);
-        }
-        else if(income >500000 && income<=1000000){
-            tax = 0.2 * (500000 - 2500000) + 0.2 * (income - 500000);
-            }
-            else {
-                tax = 0.5* (500000 - 250000) + 0.2 * (1000000 - 500000) + 0.3 * (income - 1000000);
-                }
-                printf("the total tax you need to pay is  %d", tax);
-                return 0;
-            }
+    } else if (income > 500000 && income <= 1000000) {
+        tax = 0.05 * (500000 - 250000) + 0.2 * (income - 500000);
+    } else {
+        tax = 0.05 * (500000 - 250000) + 0.2 * (1000000 - 500000) + 0.3 * (income - 1000000);
+    }
+
+    printf("The total tax you need to pay is: %.2f\n", tax); // Print tax with 2 decimal places
+    return 0;
+}
