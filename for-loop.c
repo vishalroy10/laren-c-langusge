6485eeede097bb5c10768bee2ae587a4ae55ad7a
vishalroy10 <vishalroy00004@gmail.com>
2024-12-09 09:11:27 -0800

    using for loop

---
+++ for-loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    int n = 12;
+    for(int i = 5; i<= n; i++)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
---
+++ h.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int n, i = 0;
+    scanf("%d",&n);
+    do{
+        printf("%d\n",i);
+        i++;
+
+    }
+    while(i<=n);
+return 0;
+}
